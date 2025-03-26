261e06a576066caaa8fe3f1b782e3412a30f12ee
TuhumSyai <gog.kobyakov@gmail.com>
2025-03-26 16:04:53 +0500

    Update

--- MyProject/MyApp1/views.py
+++ MyProject/MyApp1/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'contact.html')
