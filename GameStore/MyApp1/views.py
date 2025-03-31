53f9a55692eb0a4035aebffc9883c6935273de7c
TuhumSyai <gog.kobyakov@gmail.com>
2025-03-31 08:20:18 +0500

    fix

--- GameStore/MyApp1/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-def home(request):
-    return HttpResponse("Hello")
-
-# def contact(request):
-#     return render(request, 'contact.html')
