5f5b0d525e09a2b1a32016c2a3c967c79bbae8f5
TuhumSyai <gog.kobyakov@gmail.com>
2025-03-31 09:22:06 +0500

    connect API

--- GameStore/store/urls.py
+++ GameStore/store/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('game-list/', views.game_list, name="game_list")
 ]
