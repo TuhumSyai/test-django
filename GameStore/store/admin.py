c7b433d3817175497d481bcdfe8cd5eca7c73e4a
TuhumSyai <gog.kobyakov@gmail.com>
2025-04-02 16:28:51 +0500

    add models

--- GameStore/store/admin.py
+++ GameStore/store/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import User, Game, Category, Review, Order, OrderItem
 
-# Register your models here.
+# Регистрация модели User
+from django.contrib.auth.admin import UserAdmin
+admin.site.register(User, UserAdmin)
+
+# Регистрация остальных моделей
+admin.site.register(Game)
+admin.site.register(Category)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(OrderItem)
